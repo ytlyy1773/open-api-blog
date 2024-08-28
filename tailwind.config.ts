ccdc987459f1663e4e73286f45fb20c93a590a42
ytlyy <jiangwan1773@163.com>
2024-08-28 12:47:40 +0800

    [fix]  修复主题色问题

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class', // 主题色控制
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
